2ceb4619706e3660fcb65d8ee0d5b23764453833
irlstunna <issatestx@gmail.com>
2023-07-19 20:05:51 +0200

    add start script

--- app.js
+++ app.js
@@ -58,12 +58,12 @@ app.use(
   }),
 );
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
 const store = MongoStore.create({
     mongoUrl: dbUrl,
+    secret,
     touchAfter: 24 * 60 * 60,
-    crypto: {
-        secret: 'thisshouldbeabettersecret!'
-    }
 });
 
 store.on("error", function (e) {
@@ -73,7 +73,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -169,7 +169,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('Serving on port 3000')
 })
 
